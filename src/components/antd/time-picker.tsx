83500e2fda86fb86be0d5d42fafabf618ba48a86
Jairwin <jairwin@jairwin.cn>
2023-05-27 17:06:44 +0800

    feat: add f2elint

--- src/components/antd/time-picker.tsx
+++ src/components/antd/time-picker.tsx
@@ -1,14 +1,14 @@
 import { forwardRef } from "react";
 import { Dayjs } from "dayjs";
 import { PickerTimeProps } from "antd/es/date-picker/generatePicker";
-import DatePicker from "./date-picker";
+// import DatePicker from "./date-picker";
 
 export type TimePickerProps = Omit<PickerTimeProps<Dayjs>, "picker">;
 
-const TimePicker = forwardRef<any, TimePickerProps>((props, ref) => {
-  return <DatePicker {...props} picker="time" mode={undefined} ref={ref} />;
-});
+// const TimePicker = forwardRef<any, TimePickerProps>((props, ref) => {
+//   return <DatePicker {...props} picker="time" mode={undefined} ref={ref} />;
+// });
 
-TimePicker.displayName = "TimePicker";
+// TimePicker.displayName = "TimePicker";
 
-export default TimePicker;
+// export default TimePicker;
